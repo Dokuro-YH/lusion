390b02ded0bd39788251440ec7ad5ff5ddd7b920
Yanhai <shimmly@live.cn>
2019-04-29 00:25:54 +0800

    Add tide web framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(async_await, await_macro)]
+use std::io;
+
+use tide::error::{Error, ResultExt};
+use tide::{App, Context};
+
+async fn hello(ctx: Context<()>) -> Result<String, Error> {
+    let name = ctx.param::<String>("name").client_err()?;
+    Ok(format!("Hello, {}", name))
+}
+
+fn main() -> io::Result<()> {
+    let mut app = App::new(());
+    app.at("/*name").get(hello);
+    Ok(app.serve("127.0.0.1:8000")?)
 }
