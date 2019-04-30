019912a44f3e8586038e9f5e84ff2ea1cd35c5ba
Yanhai <shimmly@live.cn>
2019-05-01 01:59:12 +0800

    Move api function to graphql mod

--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -1,7 +1,14 @@
 ///! Graphql API module.
 mod humans;
 
-use crate::db::PgConn;
+use http::StatusCode;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+use tide::{Context, Response};
+
+use crate::db::{PgConn, PgPool};
+use crate::error::{Error, ErrorKind, ResultExt};
+use crate::resp;
 
 /// Graphql schema.
 pub type Schema = juniper::RootNode<'static, QueryRoot, MutationRoot>;
@@ -19,3 +26,22 @@ graphql_object!(QueryRoot: PgConn |&self| {
 graphql_object!(MutationRoot: PgConn |&self| {
     field humans() -> humans::MutationHuman { humans::MutationHuman }
 });
+
+pub async fn get_graphiql(_: Context<PgPool>) -> Response {
+    let res = graphiql_source("http://localhost:8000/graphql");
+    resp::html(StatusCode::OK, res)
+}
+
+pub async fn post_graphql(mut ctx: Context<PgPool>) -> Result<Response, Error> {
+    let pool = ctx.app_data();
+    let conn = pool.get_conn()?;
+    let schema = Schema::new(QueryRoot, MutationRoot);
+    let req: GraphQLRequest = await!(ctx.body_json()).context(ErrorKind::GraphqlError)?;
+    let res = req.execute(&schema, &conn);
+    let status = if res.is_ok() {
+        StatusCode::OK
+    } else {
+        StatusCode::BAD_REQUEST
+    };
+    Ok(resp::json(status, res))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![feature(async_await, await_macro)]
 
+///! An experimental, Web API based on async/await IO implementation.
+
 macro_rules! box_async {
     {$($t:tt)*} => {
         FutureObj::new(Box::new(async move { $($t)* }))
@@ -25,36 +27,11 @@ mod test_helpers;
 
 use std::{env, io};
 
-use http::StatusCode;
-use juniper::http::graphiql::graphiql_source;
-use juniper::http::GraphQLRequest;
-use tide::{App, Context, Response};
-
 use crate::db::PgPool;
-use crate::error::{Error, ErrorKind, ResultExt};
-use crate::graphql::{MutationRoot, QueryRoot, Schema};
+use crate::graphql::{get_graphiql, post_graphql};
 use crate::middleware::{CookieSecurityPolicy, SecurityMiddleware};
 
 static AUTH_SIGNING_KEY: &[u8] = &[0; 32];
-
-async fn graphiql(_: Context<PgPool>) -> Response {
-    let res = graphiql_source("http://localhost:8000/graphql");
-    resp::html(StatusCode::OK, res)
-}
-
-async fn graphql(mut ctx: Context<PgPool>) -> Result<Response, Error> {
-    let pool = ctx.app_data();
-    let conn = pool.get_conn()?;
-    let schema = Schema::new(QueryRoot, MutationRoot);
-    let req: GraphQLRequest = await!(ctx.body_json()).context(ErrorKind::GraphqlError)?;
-    let res = req.execute(&schema, &conn);
-    let status = if res.is_ok() {
-        StatusCode::OK
-    } else {
-        StatusCode::BAD_REQUEST
-    };
-    Ok(resp::json(status, res))
-}
 
 fn main() -> io::Result<()> {
     dotenv::dotenv().ok();
@@ -62,7 +39,7 @@ fn main() -> io::Result<()> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let db = PgPool::new(&database_url);
 
-    let mut app = App::new(db);
+    let mut app = tide::App::new(db);
     app.middleware(SecurityMiddleware::new(
         CookieSecurityPolicy::new(AUTH_SIGNING_KEY)
             .path("/")
@@ -72,8 +49,8 @@ fn main() -> io::Result<()> {
             .max_age(3600),
     ));
 
-    app.at("/graphiql").get(graphiql);
-    app.at("/graphql").post(graphql);
+    app.at("/graphiql").get(get_graphiql);
+    app.at("/graphql").post(post_graphql);
 
     Ok(app.serve("127.0.0.1:8000")?)
 }
